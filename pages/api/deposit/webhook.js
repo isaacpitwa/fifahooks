1719001d188f89e9bb349523275d7d8bcb492677
Isaac Pitwa <isaacpitwa256@gmail.com>
2022-10-20 13:29:38 +0300

    Add webhooks

--- pages/api/deposit/webhook.js
+++ pages/api/deposit/webhook.js
@@ -1,4 +1,9 @@
 export default function handler(req, res) {
     console.log(req.body);
+    const { address,amount,type} = req.body;
+    console.log("Webhook received!");
+    console.log("Wallet address: ",address);
+    console.log ("Amount: ", amount);
+    console.log("Type: ",type);
     res.status(200).json(req.body);
 }
