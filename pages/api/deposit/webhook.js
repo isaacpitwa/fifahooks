2ce2ee088df933dd4ca46d894c16c7efabcd80d7
Isaac Pitwa <isaacpitwa256@gmail.com>
2022-10-20 13:50:39 +0300

    Set up formiddable

--- pages/api/deposit/webhook.js
+++ pages/api/deposit/webhook.js
@@ -1,8 +1,22 @@
-export default function handler(req, res) {
-    const { address,amount,type} = JSON.parse(req.body);
+import formidable from 'formidable';
+
+export const config = {
+    api: {
+      bodyParser: false
+    }
+}
+export default  async (req, res) =>{
+    const data = await new Promise((resolve, reject) => {
+        const form = new formidable();
+        form.parse(req, (err, fields, files) => {
+          if (err) reject({ err })
+          resolve({ err, fields, files })
+        }) 
+      })
+    const { address,amount,type }  = data;
     console.log("Webhook received!");
     console.log("Wallet address: ",address);
     console.log ("Amount: ", amount);
     console.log("Type: ",type);
-    res.status(200).json(req.body);
+    res.status(200).json(req.data);
 }
